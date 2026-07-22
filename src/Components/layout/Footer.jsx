8fecaa918dd4c9bf132196a5cad3a156df8cb491
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-07-22 12:38:08 +0530

    refactor: rename common folder to layout

---
+++ src/Components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="text-center text-gray-600 text-sm font-medium py-3 mt-6 h-full flex items-end justify-center">
+            <h1 className="h-5">© 2026 FinDash</h1>
+        </footer>
+    )
+}
+export default Footer;

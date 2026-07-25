32299a8260aa3b4021cb1ceccaa14db3d95076e0
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-07-25 16:50:17 +0530

    feat: implement ProtectedRoute component

---
+++ src/Components/auth/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router";
+import useAuth from "../../hooks/useAuth.js";
+const ProtectedRoute = () => {
+    const { isAuthenticated, loading, user } = useAuth();
+
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    return isAuthenticated ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" replace />
+    );
+};
+
+export default ProtectedRoute;

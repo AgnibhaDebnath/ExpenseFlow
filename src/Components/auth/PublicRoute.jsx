ff7de71ceb02d151b6d07dc38cb4df836a091d83
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-07-25 16:51:06 +0530

    feat: implement PublicRoute component

---
+++ src/Components/auth/PublicRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router";
+import useAuth from "../../hooks/useAuth.js";
+const PublicRoute = () => {
+    const { isAuthenticated, loading, user } = useAuth()
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    return isAuthenticated ? (
+        <Navigate to="/dashboard" replace />
+    ) : (
+        <Outlet />
+    );
+};
+
+export default PublicRoute;

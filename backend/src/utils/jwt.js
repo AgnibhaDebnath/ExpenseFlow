5de6a81b03cfefbe0e37054f934fbe3334d71b78
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-01 14:26:49 +0530

    feat: implement JWT utility

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+
+const generateToken = (newUser) => {
+    const token = jwt.sign(
+        {
+            userID: newUser._id,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' })
+    return token;
+}
+const verifyToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+}
+export { generateToken,verifyToken };

45cbdc202e32cdffa0752d3ab1055f225100739c
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-02 13:34:00 +0530

    feat: implement NoResult component for filtered transactions

---
+++ src/Components/transactions/NoResults.jsx
@@ -0,0 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+
+const NoResults = ({ setSelectedCategory, setSelectedType }) => {
+    return (
+        <tr>
+            <td colSpan={6} className="py-6 text-center text-gray-500 ">
+                <div className="flex flex-col items-center text-center">
+                    <FaSearch
+                        size={34}
+                        className="text-gray-300 mb-4"
+                    />
+                    <h3 className="text-lg font-semibold text-gray-700 tracking-wide">No matching transactions</h3>
+                    <p className="mt-2 text-sm text-gray-500 ">Try adjusting or clearing your filters.</p>
+
+                    <p></p>
+                    <button onClick={() => {
+
+                        setSelectedCategory("all categories")
+                        setSelectedType("all types");
+                    }} className="bg-indigo-500 hover:bg-indigo-600 text-white px-6 py-1.5 rounded-lg font-medium cursor-pointer tracking-wide my-2 mt-5">Clear Filters</button>
+                </div>
+            </td>
+
+        </tr >
+    )
+}
+export default NoResults;

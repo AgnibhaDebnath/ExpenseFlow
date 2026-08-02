5e5165878dbde95e02d8a89596f4f30a0ba9bb9c
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-02 14:10:23 +0530

    feat: implement authentication middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { verifyToken } from "../utils/jwt.js"
+const authenticate = async (req, res, next) => {
+    try {
+        
+   
+    const token = req.cookies.token;
+
+        if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication required",
+      });
+    }
+        const decoded = await verifyToken(token);
+        
+        req.user = {
+            id: decoded.userID,
+        };
+
+    } catch (err) {
+        return res.status(401).json({
+            success: false,
+            message: "Token expired or invalid",
+        });
+    }
+    next();
+}
+export default authenticate

c0a055080ff5f737dec139ccf2a32d239b9bfbbb
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-05 09:23:07 +0530

    feat: implement transaction routes

---
+++ backend/src/modules/transaction/transaction.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import transactionFormValidator from "./transaction.validator.js";
+import authenticate from "../../middlewares/auth.middleware.js";
+import { createTransactionController,getTransactionsController,updateTransactionController,deleteTransactionController } from "./transaction.controller.js";
+const transactionRoutes = express.Router();
+transactionRoutes.post("/", transactionFormValidator, authenticate, createTransactionController)
+transactionRoutes.get("/", authenticate, getTransactionsController)
+transactionRoutes.put("/:id", transactionFormValidator, authenticate, updateTransactionController)
+transactionRoutes.delete("/:id", authenticate,deleteTransactionController)
+
+export default transactionRoutes

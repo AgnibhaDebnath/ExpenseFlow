9b2b05a5d341da92151fa780121b6c3cf2d38f6d
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-05 09:31:41 +0530

    feat: implement transaction form validation

---
+++ backend/src/modules/transaction/transaction.validator.js
@@ -0,0 +1,46 @@
+import { z } from "zod";
+const TransactionSchema = z.object({
+        title: z
+            .string()
+            .trim()
+        .min(1, "Title is required")
+        .max(40, "Title must be less than 30 characters"),
+
+        amount: z.preprocess(value => value == "" ? undefined : Number(value),
+            z.number({ error: "Amount is required" })
+                .positive("Amount must be greater than 0"),
+        ),
+        type: z.enum(["income", "expense"], {
+            error: "Please select a transaction type",
+        }),
+
+        category: z.enum(["food", "transport", "Shopping", "bills", "salary", "healthcare", "entertainment", "household", "education", "investment","freelance"], {
+            error: "Please select a category"
+        }),
+    date:z.coerce.date({
+            error: "Please select a date",
+        }),
+    
+    });
+
+const transactionFormValidator = (req, res, next) => {
+
+    const result = TransactionSchema.safeParse(req.body)
+
+    if (!result.success) {
+         const tree = z.treeifyError(result.error);
+            
+       return res.status(400).json({
+            errors:{
+                title: tree.properties?.title?.errors ?? [],
+                amount: tree.properties?.amount?.errors ?? [],
+                type: tree.properties?.type?.errors ?? [],
+                category: tree.properties?.category?.errors ?? [],
+                date: tree.properties?.date?.errors ?? [],
+            }
+        })
+    }
+    next()
+}
+
+export default transactionFormValidator;

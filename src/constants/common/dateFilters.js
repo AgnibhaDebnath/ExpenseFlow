8f5e890f2a160473cb261c5dcb43c19a192ab070
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-07 18:06:10 +0530

    feat: add date filter options

---
+++ src/constants/common/dateFilters.js
@@ -0,0 +1,62 @@
+import { FaCalendarDays } from "react-icons/fa6";
+
+export const dateFilters = [
+    {
+        id: 1,
+        label: "All Time",
+        value:"all-time",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+
+    },
+
+    {
+        id: 2,
+        label: "This Week",
+        value:"this-week",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id: 3,
+        label: "This Month",
+        value:"this-month",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id: 4,
+        label: "Last Month",
+        value:"last-month",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id:5,
+        label: "This Year",
+        value:"this-year",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id: 6,
+        label: "Last Year",
+        value:"last-year",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id: 7,
+        label: "Select Month",
+        value:"selec-month",
+        icon: FaCalendarDays,
+        color: "text-gray-500",
+    },
+    {
+        id: 8,
+        label: "Select Year",
+        value:"select-year",
+        icon: FaCalendarDays,
+        color: "text-gray-500", 
+    },
+];

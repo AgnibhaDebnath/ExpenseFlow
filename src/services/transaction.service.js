09bbe200b54b7d083628fa090124c9ac6c5f88ec
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-07 17:53:07 +0530

    feat: implement fetchTransactions service

---
+++ src/services/transaction.service.js
@@ -0,0 +1,26 @@
+export const fetchTransactions = async (params = "") => {
+          const url = params
+    ? `http://localhost:3001/api/transaction?${params}`
+        : `http://localhost:3001/api/transaction`;
+    
+        const res = await fetch(url,
+            {
+                credentials: "include",
+                method: "GET"
+            },
+
+        )
+        const data = await res.json();
+        
+    if (!res.ok) {
+        console.log(data)
+        const error = new Error(data.message);
+        error.status = res.status;
+        throw error
+       
+    }
+
+        const { totalTransactions, transactions } = data;
+        return { totalTransactions, transactions };
+ 
+    }
